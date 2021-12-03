8a79186e4373b34f0c1d73748e371ef19fbe1cfa
leave3310 <sampisam3@gmail.com>
2021-12-03 20:59:35 +0800

    determine flex data

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import line from "@line/bot-sdk"
+import dotenv from "dotenv-flow"
+import replyMessage from "./reply.js"
+import getUser from './other-user.js'
+import e from "express"
+dotenv.config()
+
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+}
+
+const client = new line.Client(config)
+const app = express()
+
+app.post("/callback", line.middleware(config), (req, res) => {
+  const userMessage = req.body.events[0]
+  const replyToken = req.body.events[0].replyToken
+  
+});
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
---
+++ src/other-user.js
@@ -0,0 +1,109 @@
+
+const userFlex = {
+    "type": "flex",
+    "altText": "choose your choice",
+    "contents": {
+        "type": "bubble",
+        "hero": {
+            "type": "image",
+            "url": "https://randomuser.me/api/portraits/men/75.jpg",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "size": "full",
+            "margin": "none"
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": "brad gibson",
+                    "weight": "bold",
+                    "size": "xl"
+                },
+                {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "gender",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 2
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "male",
+                                    "wrap": true,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "phone",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 2
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "011-962-7516",
+                                    "wrap": true,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                            ]
+                        },
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "email",
+                                    "color": "#aaaaaa",
+                                    "size": "sm",
+                                    "flex": 2
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "brad.gibson@example.com",
+                                    "wrap": true,
+                                    "color": "#666666",
+                                    "size": "sm",
+                                    "flex": 5
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+
+}
+
+
+
+
+
+
